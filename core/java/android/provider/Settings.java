4d95e66b51f0313b4516fed296f1d422f031106d
blunden <blunden2@gmail.com>
2012-07-30 15:31:21 +0200

    Framework: Add option to always show battery status on lockscreen (1/2)
    Based on the feature by burnsra in CM7
    Authored by Blunden
    Tweaked by DvTonder
    Change-Id: I642da4dcd82a776a74d77031d37acc76f97b25af
    
    lockscreen battery status: add off option (2/2)
    Change-Id: Ie10015865f161e783dc394d467e456d51dc9fbbc
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardUpdateMonitor.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2835,6 +2835,15 @@ public final class Settings {
         public static final String LOCKSCREEN_LID_WAKE = "lockscreen_lid_wake";
 
         /**
+         * Lockscreen battery status visibility mode
+         * 0 = show if charging
+         * 1 = always show
+         * 2 = never show
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_VISIBILITY = "lockscreen_always_show_battery";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide
@@ -4303,6 +4312,7 @@ public final class Settings {
             POWER_MENU_SOUND_ENABLED,
             POWER_MENU_USER_ENABLED,
             LOCKSCREEN_VIBRATE_ENABLED,
+            LOCKSCREEN_BATTERY_VISIBILITY,
             PHONE_BLACKLIST_ENABLED,
             PHONE_BLACKLIST_NOTIFY_ENABLED,
             PHONE_BLACKLIST_PRIVATE_NUMBER_MODE,
