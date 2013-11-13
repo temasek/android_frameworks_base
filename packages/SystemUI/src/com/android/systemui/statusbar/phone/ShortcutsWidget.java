fbed4d18be36b8d1d63301406789b1b0a93c5c65
Janson Kang <temasek71@gmail.com>
2013-11-13 10:32:02 +0800

    Refactoring

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ShortcutsWidget.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ShortcutsWidget.java
@@ -57,9 +57,9 @@ import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import com.android.internal.util.slim.ButtonsHelper;
-import com.android.internal.util.slim.ButtonConfig;
-import com.android.internal.util.slim.ButtonsConstants;
+import com.android.internal.util.temasek.ButtonsHelper;
+import com.android.internal.util.temasek.ButtonConfig;
+import com.android.internal.util.temasek.ButtonsConstants;
 import com.android.systemui.R;
 
 import java.io.File;
