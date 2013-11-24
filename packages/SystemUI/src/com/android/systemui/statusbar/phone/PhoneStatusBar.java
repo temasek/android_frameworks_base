cf802dde9fbd1b2fe23897908249d89beb47549b
0xD34D <clark@scheffsblend.com>
2013-11-24 15:30:23 -0200

    Active display [squashed] (1/2)
    Change-Id: I709196b232609a1dab8e9d7fdd0a6a655d487d93
    
    Conflicts:
    
    	core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
    	core/res/res/values/cr_strings.xml
    	core/res/res/values/cr_symbols.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -98,6 +98,8 @@ import android.widget.ImageView;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
@@ -328,8 +330,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     // last theme that was applied in order to detect theme change (as opposed
     // to some other configuration change).
     CustomTheme mCurrentTheme;
-    private boolean mRecreating = false;
-
+    
     // status bar brightness control
     private boolean mBrightnessControl;
     private float mScreenWidth;
@@ -339,6 +340,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     int mLinger;
     int mInitialTouchX;
     int mInitialTouchY;
+
+    private boolean mRecreating = false;
 
     // for disabling the status bar
     int mDisabled = 0;
@@ -778,6 +781,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             }
         } catch (RemoteException ex) {
             // no window manager? good luck with that
+        }
+
+        if (mRecreating) {
+        } else {
+            addActiveDisplayView();
         }
 
         // figure out which pixel-format to use for the status bar.
@@ -2810,7 +2818,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         mHandler.postDelayed(mUserAutohide, 350); // longer than app gesture -> flag clear
     }
 
-    private boolean areLightsOn() {
+    public boolean areLightsOn() {
         return 0 == (mSystemUiVisibility & View.SYSTEM_UI_FLAG_LOW_PROFILE);
     }
 
@@ -2830,6 +2838,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         }
     }
 
+    @Override
     public void topAppWindowChanged(boolean showMenu) {
         if (DEBUG) {
             Log.d(TAG, (showMenu?"showing":"hiding") + " the MENU button");
