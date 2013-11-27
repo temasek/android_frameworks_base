1b76fd7dd393313eb8eba4dabc5f750ef043a13b
sarbyn <sarbyn@gmail.com>
2013-11-27 13:05:20 +0100

    Development shortcut in StatusBar and Recent Panel
    It enables the wipe data and application force close option in the
    recent tasks list and in the notification area. These options will
    be available only if the Development shortcut option is enabled.
    
    Change-Id: Ia4e1d040e8eade5afe118dd62bc28c86381c03e6
    
    Conflicts:
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/cm_strings.xml
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
    
    Conflicts:
    
    	packages/SystemUI/AndroidManifest.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6074,6 +6074,13 @@ public final class Settings {
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
+         * Whether to display the 'Wipe data' and 'Force close' options in the notification
+         * area and in the recent app list
+         * @hide
+         */
+        public static final String DEVELOPMENT_SHORTCUT = "development_shortcut";
+
+        /**
          * Whether newly installed apps should run with privacy guard by default
          * @hide
          */
@@ -6131,7 +6138,8 @@ public final class Settings {
             UI_NIGHT_MODE,
             UI_INVERTED_MODE,
             ADVANCED_REBOOT,
-            PRIVACY_GUARD_DEFAULT
+            PRIVACY_GUARD_DEFAULT,
+            DEVELOPMENT_SHORTCUT
         };
 
         /**
