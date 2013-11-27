thefutureosp/android_frameworks_base-1

Frameworks: Port Lockscreen Targets and Shortcuts
    First thanks to PA and CM which gave us partly code and some ideas.
    Still some parts of the original implementation are/were buggy as well
    we needed to ehance it.

    Whatever the new Slim version is now ready and seems to be completly bug
    free.

    - Configurable lockscreen targets...on tablets 8 on phone either 5 or 8
    - configurable lockscreen shortcuts only on phones...up to 5 shortcuts
    - Centralazing some methods....using now then rounded corner on custom
      images systemwide...means on all other shorcut features we have
    - multiDpi compatible
    - new settings logic
    - instance handling of all dialogs like we do on all other places
      way nicer this way
    - tons of fixes

Change-Id: Ie62857beeacceb880efe9aa57246e111d64cd00e

core/java/com/android/internal/util/crdroid/ImageHelper.java
@@ -1,0 +1,104 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.crdroid;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
+
+public class ImageHelper {
+
+    public static Bitmap getColoredBitmap(Drawable d, int color) {
+        if (d == null) {
+            return null;
+        }
+        Bitmap colorBitmap = ((BitmapDrawable) d).getBitmap();
+        Bitmap grayscaleBitmap = toGrayscale(colorBitmap);
+        Paint pp = new Paint();
+        PorterDuffColorFilter frontFilter =
+            new PorterDuffColorFilter(color, Mode.MULTIPLY);
+        pp.setColorFilter(frontFilter);
+        Canvas cc = new Canvas(grayscaleBitmap);
+        cc.drawBitmap(grayscaleBitmap, 0, 0, pp);
+        return grayscaleBitmap;
+    }
+
+    private static Bitmap toGrayscale(Bitmap bmpOriginal) {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
+
+    public static Drawable resize(Context context, Drawable image, int size) {
+        if (image == null || context == null) {
+            return null;
+        }
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, size,
+                context.getResources().getDisplayMetrics());
+
+        Bitmap d = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmapOrig = Bitmap.createScaledBitmap(d, px, px, false);
+        return new BitmapDrawable(context.getResources(), bitmapOrig);
+    }
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            return null;
+        }
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(),
+                Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = 24;
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+}

packages/Keyguard/src/com/android/keyguard/KeyguardShortcuts.java
@@ -1,0 +1,145 @@
+/*
+ * Copyright (C) 2012 ParanoidAndroid Project
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.keyguard;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.internal.R;
+import com.android.internal.util.crdroid.ButtonsHelper;
+import com.android.internal.util.crdroid.ButtonConfig;
+import com.android.internal.util.crdroid.DeviceUtils;
+import com.android.internal.util.crdroid.LockscreenTargetUtils;
+import com.android.internal.util.crdroid.SlimActions;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+public class KeyguardShortcuts extends LinearLayout {
+
+    private static final int INNER_PADDING = 20;
+    public final static String ICON_FILE = "icon_file";
+
+    private KeyguardSecurityCallback mCallback;
+    private PackageManager mPackageManager;
+    private Context mContext;
+
+    public KeyguardShortcuts(Context context) {
+        this(context, null);
+    }
+
+    public KeyguardShortcuts(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mContext = context;
+        mPackageManager = mContext.getPackageManager();
+
+        createShortcuts();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+
+    public void setKeyguardCallback(KeyguardSecurityCallback callback) {
+        mCallback = callback;
+    }
+
+    private void createShortcuts() {
+        ArrayList<ButtonConfig> buttonsConfig = ButtonsHelper.getLockscreenShortcutConfig(mContext);
+        if (buttonsConfig.size() == 0 ||
+                !DeviceUtils.isPhone(mContext) ||
+                LockscreenTargetUtils.isEightTargets(mContext)) {
+            return;
+        }
+
+        boolean longpress = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, 1, UserHandle.USER_CURRENT) == 1;
+
+        ButtonConfig buttonConfig;
+
+        for (int j = 0; j < buttonsConfig.size(); j++) {
+            buttonConfig = buttonsConfig.get(j);
+
+            final String action = buttonConfig.getClickAction();
+            ImageView i = new ImageView(mContext);
+            int dimens = Math.round(mContext.getResources().getDimensionPixelSize(
+                    R.dimen.app_icon_size));
+            LinearLayout.LayoutParams vp =
+                    new LinearLayout.LayoutParams(dimens, dimens);
+            i.setLayoutParams(vp);
+            i.setImageDrawable(ButtonsHelper.getButtonIconImage(
+                    mContext, buttonConfig.getClickAction(), buttonConfig.getIcon()));
+
+            if (longpress) {
+                i.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        SlimActions.processAction(mContext, action, true);
+                        return true;
+                    }
+                });
+            } else {
+                i.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SlimActions.processAction(mContext, action, false);
+                    }
+                });
+            }
+            addView(i);
+            if (j+1 < buttonsConfig.size()) {
+                addSeparator();
+            }
+        }
+    }
+
+    private void addSeparator() {
+        View v = new View(mContext);
+        LinearLayout.LayoutParams vp =
+                new LinearLayout.LayoutParams(INNER_PADDING, 0);
+        v.setLayoutParams(vp);
+        addView(v);
+    }
+
+    private Drawable getDrawable(Resources res, String drawableName) {
+        int resourceId = res.getIdentifier(drawableName, "drawable", "android");
+        if (resourceId == 0) {
+            Drawable d = Drawable.createFromPath(drawableName);
+            return d;
+        } else {
+            return res.getDrawable(resourceId);
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -58,7 +58,7 @@ import android.widget.FrameLayout;
 import com.android.internal.util.crdroid.ButtonConfig;
 import com.android.internal.util.crdroid.ButtonsConstants;
 import com.android.internal.util.crdroid.ButtonsHelper;
-import com.android.internal.util.crdroid.ColorHelper;
+import com.android.internal.util.crdroid.ImageHelper;
 import com.android.internal.util.crdroid.DeviceUtils;
 import com.android.internal.util.crdroid.SlimActions;
 import com.android.internal.widget.multiwaveview.GlowPadView;
@@ -480,11 +480,14 @@ public class SearchPanelView extends FrameLayout implements
                 try {
                     Drawable customIcon;
                     if (iconFile.exists()) {
-                        customIcon = ColorHelper.resize(mContext,
-                            new BitmapDrawable(getResources(), iconFile.getAbsolutePath()), 50);
+                        customIcon = new BitmapDrawable(getResources(),
+                                        ImageHelper.getRoundedCornerBitmap(
+                                        new BitmapDrawable(getResources(),
+                                        iconFile.getAbsolutePath()).getBitmap()));
+                        customIcon = ImageHelper.resize(mContext, customIcon, 50);
                     } else {
                         customIcon = new BitmapDrawable(getResources(),
-                                    ColorHelper.getColoredBitmap(ColorHelper.resize(mContext,
+                                    ImageHelper.getColoredBitmap(ImageHelper.resize(mContext,
                                     getResources().getDrawable(getResources().getIdentifier(
                                     customIconUri.substring(
                                     ButtonsConstants.SYSTEM_ICON_IDENTIFIER.length()),
@@ -544,7 +547,7 @@ public class SearchPanelView extends FrameLayout implements
 
         if (aInfo != null && pm != null) {
             return new TargetDrawable(mResources,
-                setStateListDrawable(ColorHelper.resize(mContext, aInfo.loadIcon(pm), 50)));
+                setStateListDrawable(ImageHelper.resize(mContext, aInfo.loadIcon(pm), 50)));
         }
         return noneDrawable;
     }
@@ -553,9 +556,9 @@ public class SearchPanelView extends FrameLayout implements
         if (activityIcon == null) {
             return null;
         }
-        Drawable iconBg = ColorHelper.resize(mContext,
+        Drawable iconBg = ImageHelper.resize(mContext,
             mResources.getDrawable(R.drawable.ic_navbar_blank), 50);
-        Drawable iconBgActivated = ColorHelper.resize(mContext,
+        Drawable iconBgActivated = ImageHelper.resize(mContext,
             mResources.getDrawable(R.drawable.ic_navbar_blank_activated), 50);
         int margin = (int)(iconBg.getIntrinsicHeight() / 3);
         LayerDrawable icon = new LayerDrawable (new Drawable[] {iconBg, activityIcon});

packages/SystemUI/src/com/android/systemui/statusbar/phone/ShortcutsWidget.java
@@ -45,7 +45,7 @@ import android.widget.LinearLayout;
 import com.android.internal.util.crdroid.ButtonsHelper;
 import com.android.internal.util.crdroid.ButtonConfig;
 import com.android.internal.util.crdroid.ButtonsConstants;
-import com.android.internal.util.crdroid.ColorHelper;
+import com.android.internal.util.crdroid.ImageHelper;
 
 import com.android.systemui.R;
 
@@ -256,7 +256,7 @@ public class ShortcutsWidget extends LinearLayout {
                             && !buttonConfig.getIcon().equals(ButtonsConstants.ICON_EMPTY)
                             && mColorizeMode != 1))
                         && mColorizeMode != 3) {
-                        iv.setImageBitmap(ColorHelper.getColoredBitmap(front, mColor));
+                        iv.setImageBitmap(ImageHelper.getColoredBitmap(front, mColor));
                     } else {
                         iv.setImageDrawable(front);
                     }

policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -78,7 +78,7 @@ import java.util.List;
 import java.util.UUID;
 
 import com.android.internal.util.crdroid.ButtonConfig;
-import com.android.internal.util.crdroid.ColorHelper;
+import com.android.internal.util.crdroid.ImageHelper;
 import com.android.internal.util.crdroid.PolicyConstants;
 import com.android.internal.util.crdroid.PolicyHelper;
 import com.android.internal.util.crdroid.SlimActions;
@@ -1058,8 +1058,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 if (colorMode != 3) {
                     ImageView icon = (ImageView) itemView.findViewById(ICON_IDS[i]);
                     if (icon != null) {
-                        icon.setImageDrawable(ColorHelper.resize(mContext, new BitmapDrawable(
-                            ColorHelper.getColoredBitmap(icon.getDrawable(), iconColor)), 35));
+                        icon.setImageDrawable(ImageHelper.resize(mContext, new BitmapDrawable(
+                            ImageHelper.getColoredBitmap(icon.getDrawable(), iconColor)), 35));
                     }
                 }
                 itemView.setOnClickListener(this);

temasek/android_frameworks_base

core/java/com/android/internal/util/temasek/ImageHelper.java
@@ -1,0 +1,104 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.temasek;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
+
+public class ImageHelper {
+
+    public static Bitmap getColoredBitmap(Drawable d, int color) {
+        if (d == null) {
+            return null;
+        }
+        Bitmap colorBitmap = ((BitmapDrawable) d).getBitmap();
+        Bitmap grayscaleBitmap = toGrayscale(colorBitmap);
+        Paint pp = new Paint();
+        PorterDuffColorFilter frontFilter =
+            new PorterDuffColorFilter(color, Mode.MULTIPLY);
+        pp.setColorFilter(frontFilter);
+        Canvas cc = new Canvas(grayscaleBitmap);
+        cc.drawBitmap(grayscaleBitmap, 0, 0, pp);
+        return grayscaleBitmap;
+    }
+
+    private static Bitmap toGrayscale(Bitmap bmpOriginal) {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
+
+    public static Drawable resize(Context context, Drawable image, int size) {
+        if (image == null || context == null) {
+            return null;
+        }
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, size,
+                context.getResources().getDisplayMetrics());
+
+        Bitmap d = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmapOrig = Bitmap.createScaledBitmap(d, px, px, false);
+        return new BitmapDrawable(context.getResources(), bitmapOrig);
+    }
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            return null;
+        }
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(),
+                Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = 24;
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+}

packages/Keyguard/src/com/android/keyguard/KeyguardShortcuts.java
@@ -1,0 +1,145 @@
+/*
+ * Copyright (C) 2012 ParanoidAndroid Project
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.keyguard;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.internal.R;
+import com.android.internal.util.temasek.ButtonsHelper;
+import com.android.internal.util.temasek.ButtonConfig;
+import com.android.internal.util.temasek.DeviceUtils;
+import com.android.internal.util.temasek.LockscreenTargetUtils;
+import com.android.internal.util.temasek.SlimActions;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+public class KeyguardShortcuts extends LinearLayout {
+
+    private static final int INNER_PADDING = 20;
+    public final static String ICON_FILE = "icon_file";
+
+    private KeyguardSecurityCallback mCallback;
+    private PackageManager mPackageManager;
+    private Context mContext;
+
+    public KeyguardShortcuts(Context context) {
+        this(context, null);
+    }
+
+    public KeyguardShortcuts(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mContext = context;
+        mPackageManager = mContext.getPackageManager();
+
+        createShortcuts();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+
+    public void setKeyguardCallback(KeyguardSecurityCallback callback) {
+        mCallback = callback;
+    }
+
+    private void createShortcuts() {
+        ArrayList<ButtonConfig> buttonsConfig = ButtonsHelper.getLockscreenShortcutConfig(mContext);
+        if (buttonsConfig.size() == 0 ||
+                !DeviceUtils.isPhone(mContext) ||
+                LockscreenTargetUtils.isEightTargets(mContext)) {
+            return;
+        }
+
+        boolean longpress = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, 1, UserHandle.USER_CURRENT) == 1;
+
+        ButtonConfig buttonConfig;
+
+        for (int j = 0; j < buttonsConfig.size(); j++) {
+            buttonConfig = buttonsConfig.get(j);
+
+            final String action = buttonConfig.getClickAction();
+            ImageView i = new ImageView(mContext);
+            int dimens = Math.round(mContext.getResources().getDimensionPixelSize(
+                    R.dimen.app_icon_size));
+            LinearLayout.LayoutParams vp =
+                    new LinearLayout.LayoutParams(dimens, dimens);
+            i.setLayoutParams(vp);
+            i.setImageDrawable(ButtonsHelper.getButtonIconImage(
+                    mContext, buttonConfig.getClickAction(), buttonConfig.getIcon()));
+
+            if (longpress) {
+                i.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        SlimActions.processAction(mContext, action, true);
+                        return true;
+                    }
+                });
+            } else {
+                i.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SlimActions.processAction(mContext, action, false);
+                    }
+                });
+            }
+            addView(i);
+            if (j+1 < buttonsConfig.size()) {
+                addSeparator();
+            }
+        }
+    }
+
+    private void addSeparator() {
+        View v = new View(mContext);
+        LinearLayout.LayoutParams vp =
+                new LinearLayout.LayoutParams(INNER_PADDING, 0);
+        v.setLayoutParams(vp);
+        addView(v);
+    }
+
+    private Drawable getDrawable(Resources res, String drawableName) {
+        int resourceId = res.getIdentifier(drawableName, "drawable", "android");
+        if (resourceId == 0) {
+            Drawable d = Drawable.createFromPath(drawableName);
+            return d;
+        } else {
+            return res.getDrawable(resourceId);
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -58,7 +58,7 @@ import android.widget.FrameLayout;
 import com.android.internal.util.temasek.ButtonConfig;
 import com.android.internal.util.temasek.ButtonsConstants;
 import com.android.internal.util.temasek.ButtonsHelper;
-import com.android.internal.util.temasek.ColorHelper;
+import com.android.internal.util.temasek.ImageHelper;
 import com.android.internal.util.temasek.DeviceUtils;
 import com.android.internal.util.temasek.SlimActions;
 import com.android.internal.widget.multiwaveview.GlowPadView;
@@ -480,11 +480,14 @@ public class SearchPanelView extends FrameLayout implements
                 try {
                     Drawable customIcon;
                     if (iconFile.exists()) {
-                        customIcon = ColorHelper.resize(mContext,
-                            new BitmapDrawable(getResources(), iconFile.getAbsolutePath()), 50);
+                        customIcon = new BitmapDrawable(getResources(),
+                                        ImageHelper.getRoundedCornerBitmap(
+                                        new BitmapDrawable(getResources(),
+                                        iconFile.getAbsolutePath()).getBitmap()));
+                        customIcon = ImageHelper.resize(mContext, customIcon, 50);
                     } else {
                         customIcon = new BitmapDrawable(getResources(),
-                                    ColorHelper.getColoredBitmap(ColorHelper.resize(mContext,
+                                    ImageHelper.getColoredBitmap(ImageHelper.resize(mContext,
                                     getResources().getDrawable(getResources().getIdentifier(
                                     customIconUri.substring(
                                     ButtonsConstants.SYSTEM_ICON_IDENTIFIER.length()),
@@ -544,7 +547,7 @@ public class SearchPanelView extends FrameLayout implements
 
         if (aInfo != null && pm != null) {
             return new TargetDrawable(mResources,
-                setStateListDrawable(ColorHelper.resize(mContext, aInfo.loadIcon(pm), 50)));
+                setStateListDrawable(ImageHelper.resize(mContext, aInfo.loadIcon(pm), 50)));
         }
         return noneDrawable;
     }
@@ -553,9 +556,9 @@ public class SearchPanelView extends FrameLayout implements
         if (activityIcon == null) {
             return null;
         }
-        Drawable iconBg = ColorHelper.resize(mContext,
+        Drawable iconBg = ImageHelper.resize(mContext,
             mResources.getDrawable(R.drawable.ic_navbar_blank), 50);
-        Drawable iconBgActivated = ColorHelper.resize(mContext,
+        Drawable iconBgActivated = ImageHelper.resize(mContext,
             mResources.getDrawable(R.drawable.ic_navbar_blank_activated), 50);
         int margin = (int)(iconBg.getIntrinsicHeight() / 3);
         LayerDrawable icon = new LayerDrawable (new Drawable[] {iconBg, activityIcon});

packages/SystemUI/src/com/android/systemui/statusbar/phone/ShortcutsWidget.java
@@ -45,7 +45,7 @@ import android.widget.LinearLayout;
 import com.android.internal.util.temasek.ButtonsHelper;
 import com.android.internal.util.temasek.ButtonConfig;
 import com.android.internal.util.temasek.ButtonsConstants;
-import com.android.internal.util.temasek.ColorHelper;
+import com.android.internal.util.temasek.ImageHelper;
 
 import com.android.systemui.R;
 
@@ -256,7 +256,7 @@ public class ShortcutsWidget extends LinearLayout {
                             && !buttonConfig.getIcon().equals(ButtonsConstants.ICON_EMPTY)
                             && mColorizeMode != 1))
                         && mColorizeMode != 3) {
-                        iv.setImageBitmap(ColorHelper.getColoredBitmap(front, mColor));
+                        iv.setImageBitmap(ImageHelper.getColoredBitmap(front, mColor));
                     } else {
                         iv.setImageDrawable(front);
                     }

policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -78,7 +78,7 @@ import java.util.List;
 import java.util.UUID;
 
 import com.android.internal.util.temasek.ButtonConfig;
-import com.android.internal.util.temasek.ColorHelper;
+import com.android.internal.util.temasek.ImageHelper;
 import com.android.internal.util.temasek.PolicyConstants;
 import com.android.internal.util.temasek.PolicyHelper;
 import com.android.internal.util.temasek.SlimActions;
@@ -1058,8 +1058,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 if (colorMode != 3) {
                     ImageView icon = (ImageView) itemView.findViewById(ICON_IDS[i]);
                     if (icon != null) {
-                        icon.setImageDrawable(ColorHelper.resize(mContext, new BitmapDrawable(
-                            ColorHelper.getColoredBitmap(icon.getDrawable(), iconColor)), 35));
+                        icon.setImageDrawable(ImageHelper.resize(mContext, new BitmapDrawable(
+                            ImageHelper.getColoredBitmap(icon.getDrawable(), iconColor)), 35));
                     }
                 }
                 itemView.setOnClickListener(this);