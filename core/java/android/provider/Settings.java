cb2452ed2c7b3b85f88340cd4af81b5bdda4d058
David96 <xd_e-mail@gmx.de>
2013-12-12 14:45:33 +0100

    Lockscreen Blur (1/2)
    Revert "Revert "Lockscreen Blur""
    
    We want to keep this.
    This reverts commit dc82b58247e4946acd8ae497a65fd79b0b61ebb9.
    
    Change-Id: I125d911e9e868e2b568876882b252e3375d66e78
    
    Revert "Revert: Lockscreen blur": fix derp
    
    Change-Id: I1dde45b623d8ed13168e68c666d3e0c7fef293ce
    
    Lockscreen Blur: fix old nav/status bar shining through on lockscreen
    
    Fix lockscreen blur on xxhdpi devices (thanks @Curtis Mayers)
    
    All credits to Curtis Mayers: https://gerrit.paranoidandroid.co/#/c/382/
    
    Fix lockscreen blur not working on flo
    
    Fix: Correct possible null pointer when enabling see through lockscreen
    
    PS2: Remove unused variable
    
    Change-Id: I28884cb18964b273539261b8c0f72cedf2e60666
    
    Fix coverflow problem caused by lockscreen blur
    
    KeyguardViewManager: use cached see through setting
    
    When merging PA remote repo, please keep in mind not to use "isSeeThroughEnabled()" but "mSeeThrough".
    
    Merge PA's lockscreen blur without loosing our configurability
    
    Change-Id: Id400b797804bea0fd63bb4fe6135f0966e813a05
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardServiceWrapper.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4085,6 +4085,14 @@ public final class Settings {
         public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
 
         /**
+         * Allows blurring the lockscreen background
+         * @hide
+         */
+
+        public static final String LOCKSCREEN_BLUR_BEHIND = "lockscreen_blur_behind";
+        public static final String LOCKSCREEN_BLUR_RADIUS = "lockscreen_blur_radius";
+
+        /**
          * Whether the lockscreen vibrate should be enabled.
          * @hide
          */
