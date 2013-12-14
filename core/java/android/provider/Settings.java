77ecbb3f7f3fe868b21bdf4b4bd7464db13d54bc
Jubakuba <Jubakuba@gmail.com>
2013-12-14 04:01:57 -0700

    Framework: Chamber of Secrets (1/2) [WIP]
    PS5: Add long settings.
    PS6: Remove Chamber if setting
    	not active. Also prevent
    	alert from retracting
    	when device is rotated.
    PS7: Whitespace
    PS8: Add color-parsing to integers.
    	Allows you to use "red",
    	"green", "#ff0099cc", etc
    	as input-values.  Fancy.
    PS9: String Change
    PS10: Null string check.
    PS11: Null string ::: don't process code.
    
    Change-Id: I47d85ddcfb71c207a983584e08166bc85ab7d588

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5188,6 +5188,12 @@ public final class Settings {
         public static final String LOCK_SCREEN_POWER_MENU = "lock_screen_power_menu";
 
         /**
+         * Chamber on / off (custom setting shortcuts)
+         * @hide
+         */
+        public static final String CHAMBER_OF_SECRETS = "chamber_of_secrets";
+
+        /**
          * The Logging ID (a unique 64-bit value) as a hex string.
          * Used as a pseudonymous identifier for logging.
          * @deprecated This identifier is poorly initialized and has
