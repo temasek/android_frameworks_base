20f19b50e0ad365eb80ab5c001fd859d2af5572d
Lars Greiss <kufikugel@googlemail.com>
2013-12-17 14:09:56 +0100

    QS: add theme switcher tile (TRDS) 1/2
    this adds a theme switcher tile
    
    normal click: turn on/off dark/light theme
    longpress: switch between the auto modes (manual/light sensor/twilight)
    
    Change-Id: I62b8e11b5714c179ccbf1c1e341b47c84da0fb56
    
    Conflicts:
    
    	core/java/com/android/internal/util/temasek/QSConstants.java
    	packages/SystemUI/res/values/cr_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -45,6 +45,7 @@ import static com.android.internal.util.temasek.QSConstants.TILE_SCREENTIMEOUT;
 import static com.android.internal.util.temasek.QSConstants.TILE_SETTINGS;
 import static com.android.internal.util.temasek.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.temasek.QSConstants.TILE_SYNC;
+import static com.android.internal.util.temasek.QSConstants.TILE_THEME;
 import static com.android.internal.util.temasek.QSConstants.TILE_TORCH;
 import static com.android.internal.util.temasek.QSConstants.TILE_USBTETHER;
 import static com.android.internal.util.temasek.QSConstants.TILE_USER;
@@ -95,6 +96,7 @@ import com.android.systemui.quicksettings.RingerModeTile;
 import com.android.systemui.quicksettings.ScreenTimeoutTile;
 import com.android.systemui.quicksettings.SleepScreenTile;
 import com.android.systemui.quicksettings.SyncTile;
+import com.android.systemui.quicksettings.ThemeTile;
 import com.android.systemui.quicksettings.ToggleLockscreenTile;
 import com.android.systemui.quicksettings.TorchTile;
 import com.android.systemui.quicksettings.UsbTetherTile;
@@ -251,6 +253,8 @@ public class QuickSettingsController {
                 qs = new CustomTile(mContext, this, findCustomKey(tile));
             } else if (tile.contains(TILE_CONTACT)) {
                 qs = new ContactTile(mContext, this, findCustomKey(tile));
+            } else if (tile.equals(TILE_THEME)) {
+                qs = new ThemeTile(mContext, this);
             }
 
             if (qs != null) {
