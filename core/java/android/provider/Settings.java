9c7d9b58ff6392331e169323072d97dda90993aa
Muhammed Nazim <mnm9994u@gmail.com>
2013-12-27 02:21:20 -0500

    Return: Custom Carrier Label [1/2]
    PS 3 : Fix code style
    
    Change-Id: Ic865e3c5204ab05f1e9ab0e081a54b155b577086
    
    Patchset: 4
    https://gerrit.slimroms.net/#/c/1849/

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4205,6 +4205,13 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Holds the text for the Carrier label. An empty string
+         * will bring the default text back.
+         * @hide
+         */
+        public static final String NOTIFICATION_CUSTOM_CARRIER_LABEL = "notification_custom_carrier_label";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
