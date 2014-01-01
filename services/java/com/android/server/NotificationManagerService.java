2d8a20fe572285b51224e02b7d0230967504acfb
Sam Mortimer <sam@mortimer.me.uk>
2013-12-31 16:22:05 -0800

    [2/2] Framework: instant led test
    Adds support a new notification extra boolean
    EXTRA_FORCE_SHOW_LIGHTS.  Used by settings
    notification light picker to force lights on
    when the screen is on.
    
    Change-Id: If0a42d32b28fe8c02ef5f7dd148db7eb478fac17
    
    Conflicts:
    
    	services/java/com/android/server/NotificationManagerService.java

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -1463,6 +1463,9 @@ public class NotificationManagerService extends INotificationManager.Stub
                 // turn off LED when user passes through lock screen
                 if (!mDreaming && !ScreenOnNotificationLed) { 
                     mNotificationLight.turnOff();
+                    if (mLedNotification == null || !isLedNotificationForcedOn(mLedNotification)) {
+                        mNotificationLight.turnOff();
+                    }
                 }
             } else if (action.equals(Intent.ACTION_USER_SWITCHED)) {
                 // reload per-user settings
@@ -2583,6 +2586,16 @@ public class NotificationManagerService extends INotificationManager.Stub
         }
     }
 
+    private boolean isLedNotificationForcedOn(NotificationRecord r) {
+        if (r != null) {
+            final Notification n = r.sbn.getNotification();
+            if (n.extras != null) {
+                return n.extras.getBoolean(Notification.EXTRA_FORCE_SHOW_LIGHTS, false);
+            }
+        }
+        return false;
+    }
+
     // lock on mNotificationList
     private void updateLightsLocked() {
         boolean ScreenOnNotificationLed = Settings.System.getInt(mContext.getContentResolver(),
@@ -2601,8 +2614,21 @@ public class NotificationManagerService extends INotificationManager.Stub
 
         // Don't flash while we are in a call, screen is on or we are
         // in quiet hours with light dimmed
-        if (mLedNotification == null || mInCall || (mScreenOn && !ScreenOnNotificationLed && !mDreaming) 
-            	|| (QuietHoursUtils.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM))) { 
+        // (unless Notification has EXTRA_FORCE_SHOW_LGHTS)
+        final boolean enableLed;
+        if (mLedNotification == null) {
+            enableLed = false;
+        } else if (isLedNotificationForcedOn(mLedNotification)) {
+            enableLed = true;
+        } else if (mInCall || (mScreenOn && !ScreenOnNotificationLed && !mDreaming)) {
+            enableLed = false;
+        } else if (QuietHoursUtils.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM)) {
+            enableLed = false;
+        } else {
+            enableLed = true;
+        }
+
+        if (!enableLed) {
             mNotificationLight.turnOff();
         } else if (mNotificationPulseEnabled) {
             final Notification ledno = mLedNotification.sbn.getNotification();
