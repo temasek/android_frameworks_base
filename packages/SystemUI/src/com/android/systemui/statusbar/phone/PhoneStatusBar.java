b5f2b8caf88f0cc3385eacff591e65e9c39b653a
beerbong <yghazi@gmail.com>
2014-01-05 22:10:46 -0200

    [2/2] Frameworks: HALO (Updated patchset 4 -> 9)
    This is an update from my previous port.
    My last port was patchset: 4
    
    Remove HALO Master Switch
    
    Forward port from jb.
    PS1: Initial port Fix conficts with notification LED Fix derp Major derp HALO Mods Rebase Fix blackist, code clean and added some TODOs
    PS2: Fix build
    PS3: Fix bubble sometimes doesn't respond on new notification
    PS4: Align halo icon
    PS5: Keep floating app in recents and disable transparent layer temporarily (NOT INCLUDED)
    PS6: Fix conflict after recent merges
    PS7: Cosmetic fixes: Fix buttons layout in status bar. Status bar button holders show halo + qs switch on notification and qs edit mode + notification switch on qs. Update new margins size. (NOT INCLUDED)
    PS8: Rebase Clean white spaces Toggle on/off from notification shade icon (remove red X) Standardize behavior (remove option to reverse tap/double tap)
    PS9: Remove references to LayerActivity
    
    Change-Id: I4c5e973752536e92c38e74eae59e4ae0e7ceaa8a
    
    Conflicts:
    
    	core/java/android/app/Activity.java
    	core/java/android/app/INotificationManager.aidl
    	core/java/android/provider/Settings.java
    	core/res/res/values/cr_symbols.xml
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/drawable-hdpi/ic_notify_halo_normal.png
    	packages/SystemUI/res/drawable-hdpi/ic_notify_halo_pressed.png
    	packages/SystemUI/res/drawable-mdpi/ic_notify_halo_normal.png
    	packages/SystemUI/res/drawable-mdpi/ic_notify_halo_pressed.png
    	packages/SystemUI/res/drawable-xhdpi/ic_notify_halo_normal.png
    	packages/SystemUI/res/drawable-xhdpi/ic_notify_halo_pressed.png
    	packages/SystemUI/res/layout/status_bar_expanded_header.xml
    	packages/SystemUI/res/values/cr_dimens.xml
    	packages/SystemUI/res/values/cr_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/HaloProperties.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	services/java/com/android/server/NotificationManagerService.java
    	services/java/com/android/server/am/ActivityRecord.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -633,8 +633,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 mSettingsButton.setVisibility(userSetup ? View.VISIBLE : View.INVISIBLE);
             }
             if (mHaloButton != null && mHasFlipSettings) {
-                mHaloButtonVisible = userSetup;
-                updateHaloButton();
+                mHaloButton.setVisibility(userSetup ? View.VISIBLE : View.INVISIBLE);
             }
             if (mSettingsPanel != null) {
                 mSettingsPanel.setEnabled(userSetup);
@@ -898,8 +897,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mHaloButton = (ImageView) mStatusBarWindow.findViewById(R.id.halo_button);
         if (mHaloButton != null) {
             mHaloButton.setOnClickListener(mHaloButtonListener);
-            mHaloButtonVisible = true;
-            updateHaloButton();
+            mHaloButton.setVisibility(View.VISIBLE);
         }
 
         if (mHasFlipSettings) {
@@ -1603,6 +1601,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             ((ImageView)mClearButton).setImageResource(R.drawable.ic_notify_clear);
         }
 
+	if (mHaloButton != null) {
+            mHaloButton.setImageDrawable(null);
+            mHaloButton.setImageResource(mHaloActive
+                            ? R.drawable.ic_notify_halo_normal
+                            : R.drawable.ic_notify_halo_pressed);
+        }
+
         if (mSettingsButton != null) {
             // Force asset reloading
             mSettingsButton.setImageDrawable(null);
@@ -2269,6 +2274,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mSettingsButtonAnim = start(
             ObjectAnimator.ofFloat(mSettingsButton, View.ALPHA, 1f)
                 .setDuration(FLIP_DURATION));
+        mHaloButton.setVisibility(View.VISIBLE);
+        mHaloButtonAnim = start(
+            ObjectAnimator.ofFloat(mHaloButton, View.ALPHA, 1f)
+                .setDuration(FLIP_DURATION));
         mClearButton.setVisibility(View.VISIBLE);
         mClearButton.setAlpha(0f);
         setAreThereNotifications(); // this will show/hide the button as necessary
@@ -2319,8 +2328,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mFlipSettingsView.setScaleX(1f);
         mFlipSettingsView.setVisibility(View.VISIBLE);
         mSettingsButton.setVisibility(View.GONE);
-        mHaloButtonVisible = false;
-        updateHaloButton();
+        mHaloButton.setVisibility(View.GONE);
         mScrollView.setVisibility(View.GONE);
         mScrollView.setScaleX(0f);
         mNotificationButton.setVisibility(View.VISIBLE);
@@ -2482,8 +2490,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mSettingsButton.setAlpha(1f);
             mSettingsButton.setVisibility(View.VISIBLE);
             mHaloButton.setAlpha(1f);
-            mHaloButtonVisible = true;
-            updateHaloButton();
+            mHaloButton.setVisibility(View.VISIBLE);
             mNotificationPanel.setVisibility(View.GONE);
             mFlipSettingsView.setVisibility(View.GONE);
             mNotificationButton.setVisibility(View.GONE);
@@ -3052,6 +3059,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 mTickerView.setVisibility(View.GONE);
                 // we do not animate the ticker away at this point, just get rid of it (b/6992707)
             }
+            mTickerView.setVisibility(View.GONE);
+            // we do not animate the ticker away at this point, just get rid of it (b/6992707)
+	    if (!mHaloActive) {
+                mStatusBarContents.setVisibility(View.VISIBLE);
+                mCenterClockLayout.setVisibility(View.VISIBLE);
+                mTickerView.setVisibility(View.GONE);
+                mStatusBarContents.startAnimation(loadAnim(com.android.internal.R.anim.fade_in, null));
+                // we do not animate the ticker away at this point, just get rid of it (b/6992707)
+            }
         }
     }
 
@@ -3378,13 +3394,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private View.OnClickListener mHaloButtonListener = new View.OnClickListener() {
         public void onClick(View v) {
-            // Activate HALO
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.HALO_ACTIVE, 1);
-            // Switch off regular ticker
-            mTickerView.setVisibility(View.GONE);
             // Collapse
             animateCollapsePanels();
+
+            // Toggle HALO
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_ACTIVE, mHaloActive ? 0 : 1);
+
+            updateHalo();
+
+            // Switch off regular ticker
+            mTickerView.setVisibility(View.GONE);
         }
     };
 
