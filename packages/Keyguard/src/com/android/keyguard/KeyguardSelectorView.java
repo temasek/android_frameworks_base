d114c426b8f09abce262c1f5b49eb146a5d8f46e
baked <travp624@gmail.com>
2014-01-09 22:20:16 -0600

    Add double tap sleep feature to secure lockscreens.
    In its current state double tap works on the status bar on unsecure
    lockscreens this adds the feature to pin, password, and pattern
    lockscreens.
    
     - pin and password: double tap the text entry view.
     - pattern: double tap anywhere on the pattern view.
    
    Patch Set 3:
     - add to slide lockscreen(glowpad): double tap glow pad to sleep
     - update commit message
    
    Change-Id: I70eeae4acaa61b44feab661082bcfdbac7b9dfc3
    Signed-off-by: baked <travp624@gmail.com>
    
    Conflicts:
    
    	packages/Keyguard/src/com/android/keyguard/KeyguardAbsKeyInputView.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
@@ -46,6 +46,8 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.util.Slog;
 import android.util.TypedValue;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.Gravity;
@@ -97,6 +99,7 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
     private int mTargetOffset;
     private float mBatteryLevel;
     private int mTaps;
+    private GestureDetector mDoubleTapGesture;
 
     OnTriggerListener mOnTriggerListener = new OnTriggerListener() {
 
@@ -340,6 +343,26 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
                 mContext.getContentResolver(),
                 Settings.System.LOCKSCREEN_GLOWPAD_TORCH, 0,
                 UserHandle.USER_CURRENT) == 1;
+
+        mDoubleTapGesture = new GestureDetector(mContext,
+                new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+                if (pm != null) pm.goToSleep(e.getEventTime());
+                return true;
+            }
+        });
+
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0) == 1) {
+            mGlowPadView.setOnTouchListener(new OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    return mDoubleTapGesture.onTouchEvent(event);
+                }
+            });
+        }
     }
 
     public void setCarrierArea(View carrierArea) {
