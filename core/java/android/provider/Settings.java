d55a58d1dfc1ad7a8963d117530722f04c8025a2
Janson Kang <temasek71@gmail.com>
2014-01-13 13:07:38 +0800

    Revert "[1/2] base: allow disabling call end sound"
    This reverts commit 029fd6226240622207f18991a7ff368e5a3613a7.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4018,13 +4018,6 @@ public final class Settings {
         public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
 
         /**
-         * Should call status sounds be player
-         *
-         * @hide
-         */
-        public static final String CALL_END_SOUND = "call_end_sound";
-
-        /**
           * Volume keys control cursor in text fields (default is 0)
           * 0 - Disabled
           * 1 - Volume up/down moves cursor left/right
