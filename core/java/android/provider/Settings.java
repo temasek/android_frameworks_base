205774d63a85799689de82befd8664ae2efc9045
maxwen <max.weninger@gmail.com>
2014-01-19 23:06:31 +0100

    [1/2] Frameworks: Wakelock Blocker
    Change-Id: Id4fd1d8da6ed40517094a5a156f7c6fea622e985

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3754,6 +3754,18 @@ public final class Settings {
         public static final String LOCKSCREEN_MAXIMIZE_WIDGETS = "lockscreen_maximize_widgets";
 
         /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+        /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
          * Performance profile
          * @see config_perf_profile_prop in frameworks/base/core/res/res/values/config.xml
          * @hide
