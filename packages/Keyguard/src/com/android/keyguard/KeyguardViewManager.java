af0c88ba5379bcba6e6bfcc397ab90be41469ba2
David96 <xd_e-mail@gmx.de>
2014-02-22 14:54:40 -0300

    Lockscreen Notifications: dismiss all button, improve animations, cleanup
    Change-Id: I77665e37d823c619680312b73d5ebb1e6e437ffd
    
    Conflicts:
    
    	core/java/com/android/internal/statusbar/IStatusBar.aidl
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -895,7 +895,7 @@ public class KeyguardViewManager {
     }
 
     public void dispatchButtonClick(int buttonId) {
-        mNotificationView.showAllNotifications();
+        mNotificationView.onButtonClick(buttonId);
     }
 
     public void launchCamera() {
