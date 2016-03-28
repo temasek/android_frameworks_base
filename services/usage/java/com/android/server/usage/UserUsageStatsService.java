19a33b12f89d597ae2c2fd1a4b82b62db234cfe5
fei.liu <fei.liu@spreadtrum.com>
2016-03-28 14:32:44 +0800

    When Monkey test, ANR in android:ui
    (android/com.android.internal.app.ChooserActivity).
    
    Root-Cause:
    in daily usage event file, too many activity resume/pause events or configuration change during monkey test.
    
    Solution:
    Just query weekly usage events when monkey test.
    
    Change-Id: I492d8d8fc0734ec3115b44b0b8da4d15809cb350

--- services/usage/java/com/android/server/usage/UserUsageStatsService.java
+++ services/usage/java/com/android/server/usage/UserUsageStatsService.java
@@ -16,6 +16,7 @@
 
 package com.android.server.usage;
 
+import android.app.ActivityManager;
 import android.app.usage.ConfigurationStats;
 import android.app.usage.TimeSparseArray;
 import android.app.usage.UsageEvents;
@@ -294,9 +295,13 @@ class UserUsageStatsService {
         if (intervalType == UsageStatsManager.INTERVAL_BEST) {
             intervalType = mDatabase.findBestFitBucket(beginTime, endTime);
             if (intervalType < 0) {
-                // Nothing saved to disk yet, so every stat is just as equal (no rollover has
-                // occurred.
-                intervalType = UsageStatsManager.INTERVAL_DAILY;
+                if(ActivityManager.isUserAMonkey()) {
+                    intervalType = UsageStatsManager.INTERVAL_WEEKLY;
+                } else {
+                    // Nothing saved to disk yet, so every stat is just as equal (no rollover has
+                    // occurred.
+                    intervalType = UsageStatsManager.INTERVAL_DAILY;
+                }
             }
         }
 
